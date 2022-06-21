3b2567576f3b91fe0f758906796aba0d86777785
Marco Fercia <marcofernandezgarciaa@gmail.com>
2022-06-21 16:55:41 +0200

    spot

---
+++ Assets/Scripts/EnemyLife.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour
+{
+
+    public int Health = 1;
+
+   public void OnSpotLight(int _num)
+    {
+        Health -= _num;
+
+        if (Health <= 0)
+        {
+            //muerto
+        }
+    }
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
