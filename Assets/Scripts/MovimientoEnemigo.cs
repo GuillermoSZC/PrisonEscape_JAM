f367879c64dd14be30c1c11c148fd74fa897fdb2
Guillermo Suárez Calleja <guillermo.suarez@live.u-tad.com>
2022-06-21 17:41:56 +0200

    Movimiento enemigo mejora

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class MovimientoEnemigo : MonoBehaviour
 {
-    public GameObject target;
-    public float speed;
+    public float limitRandomSpeed;
+
+
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        speed = Random.Range(2.0f, limitRandomSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        // Move to target
+        // transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
 
-        if(transform.position == target.transform.position)
+        transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y, transform.position.z);
+
+
+        if(transform.position.x <= -8.3)
 		{
-            Destroy(gameObject);
+			Destroy(gameObject);
 		}
-    }
+	}
 }
