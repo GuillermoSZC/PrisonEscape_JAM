a35732c1a4225c48de683d9eabb7a37060ff9916
Guillermo Suárez Calleja <guillermo.suarez@live.u-tad.com>
2022-06-21 18:48:42 +0200

    Movimiento enemigo - borracho

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -5,34 +5,58 @@ using UnityEngine;
 public class MovimientoEnemigo : MonoBehaviour
 {
 	private float speed;
-	private float randomMovement;
+	private int randomMovement;
+	private float contador = 0.0f;
+	private bool flipFlop = false;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		speed = Random.Range(2.0f, 9.0f);
+		randomMovement = Random.Range(0, 3);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		// Move to target
-		// transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-		randomMovement = Random.Range(0.0f, 10.0f);
+		EnemyMovement();
+		
 
-		if (randomMovement <= 5.0f)
-		{
-			transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y + 0.007f, transform.position.z);
-		}
-		else
-		{
-			transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y - 0.007f, transform.position.z);
-		}
-
-
-		if (transform.position.x <= -8.3)
+		if (transform.position.x <= -10.0)
 		{
 			Destroy(gameObject);
 		}
 	}
+
+	private void EnemyMovement()
+	{
+		if(randomMovement == 0)
+		{
+			if (contador == 0.0f)
+			{
+				flipFlop = true;
+			}
+			else if (contador == 300.0f)
+			{
+				flipFlop = false;
+			}
+
+			if (flipFlop)
+			{
+				contador++;
+				transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y + Time.deltaTime * speed, transform.position.z);
+			}
+			else
+			{
+				contador--;
+				transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y - Time.deltaTime * speed, transform.position.z);
+			}
+		}
+		else
+		{
+			transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y, transform.position.z);
+		}
+	}
+
 }
+
