d649a3ab2e61653ea6fd2bdfd1bb56397e4f35c6
Marco Fercia <marcofernandezgarciaa@gmail.com>
2022-06-21 20:26:42 +0200

    cosas

--- Assets/Scripts/LightSpot.cs
+++ Assets/Scripts/LightSpot.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class LightSpot : MonoBehaviour
 {
     // Start is called before the first frame update
-//     void Start()
-//     {
-//         
-//     }
-// 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         
-//     }
+    //     void Start()
+    //     {
+    //         
+    //     }
+    // 
+    //     // Update is called once per frame
+    //     void Update()
+    //     {
+    //         
+    //     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-      EnemyLife comp = other.GetComponent<EnemyLife>();
+        EnemyLife comp = other.GetComponent<EnemyLife>();
         if (comp)
         {
             comp.OnSpotLight(1);
