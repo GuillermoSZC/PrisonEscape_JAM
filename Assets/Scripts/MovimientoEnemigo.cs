73a911b13746c7afb6a7db52e9acbf00d3e59f90
Guillermo Suárez Calleja <guillermo.suarez@live.u-tad.com>
2022-06-21 17:58:55 +0200

    movimiento random

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class MovimientoEnemigo : MonoBehaviour
 {
-    public float limitRandomSpeed;
+	private float speed;
+	private float randomMovement;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		speed = Random.Range(2.0f, 9.0f);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Move to target
+		// transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+		randomMovement = Random.Range(0.0f, 10.0f);
+
+		if (randomMovement <= 5.0f)
+		{
+			transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y + 0.007f, transform.position.z);
+		}
+		else
+		{
+			transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y - 0.007f, transform.position.z);
+		}
 
 
-    private float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        speed = Random.Range(2.0f, limitRandomSpeed);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Move to target
-        // transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-
-        transform.position = new Vector3(transform.position.x - Time.deltaTime * speed, transform.position.y, transform.position.z);
-
-
-        if(transform.position.x <= -8.3)
+		if (transform.position.x <= -8.3)
 		{
 			Destroy(gameObject);
 		}
