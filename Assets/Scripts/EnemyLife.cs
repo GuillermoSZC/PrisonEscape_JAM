84949b7c10d5aaa5b99392544e5ba566ce86a39e
Marco Fercia <marcofernandezgarciaa@gmail.com>
2022-06-21 17:42:08 +0200

    destroy cubes

--- Assets/Scripts/EnemyLife.cs
+++ Assets/Scripts/EnemyLife.cs
@@ -14,6 +14,7 @@ public class EnemyLife : MonoBehaviour
         if (Health <= 0)
         {
             //muerto
+            Destroy(gameObject);
         }
     }
 
--- Assets/Scripts/LightSpot.cs
+++ Assets/Scripts/LightSpot.cs
@@ -2,24 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpotLight : MonoBehaviour
+public class LightSpot : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//     void Start()
+//     {
+//         
+//     }
+// 
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         
+//     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        other.GetComponent<EnemyLife>().OnSpotLight(5);
+      EnemyLife comp = other.GetComponent<EnemyLife>();
+        if (comp)
+        {
+            comp.OnSpotLight(1);
+        }
     }
 
 
