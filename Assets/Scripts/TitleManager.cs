25fbd179410f08c4b5585ec594720293a378e7d2
Marco Fercia <marcofernandezgarciaa@gmail.com>
2022-06-22 20:28:23 +0200

    efwpoj

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-	public GameObject transitionBegin;
-	public GameObject transitionEnd;
-	public GameObject tutoImage;
-	private AudioSource aSource;
+    public GameObject transitionBegin;
+    public GameObject transitionEnd;
+    public GameObject tutoImage;
+    public Text record;
+    private AudioSource aSource;
 
-	private void Start()
-	{
-		Time.timeScale = 1;
-		transitionBegin.SetActive(true);
-		tutoImage.SetActive(false);
-		transitionEnd.SetActive(false);
+    private void Start()
+    {
+        Time.timeScale = 1;
+        transitionBegin.SetActive(true);
+        tutoImage.SetActive(false);
+        transitionEnd.SetActive(false);
 		aSource = gameObject.GetComponent<AudioSource>();
-	}
+        record.text = PlayerPrefs.GetFloat("score", 0000.0f).ToString();
+    }
 
 	public void botonjugar()
 	{
