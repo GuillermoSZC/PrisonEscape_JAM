4da60d8ee3b9e91da266a76582151ef2e281160b
Guillermo Suárez Calleja <guillermo.suarez@live.u-tad.com>
2022-06-22 20:23:51 +0200

    SONIDITOS 2

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -5,35 +5,43 @@ using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-    public GameObject transitionBegin;
-    public GameObject transitionEnd;
-    public GameObject tutoImage;
+	public GameObject transitionBegin;
+	public GameObject transitionEnd;
+	public GameObject tutoImage;
+	private AudioSource aSource;
 
-    private void Start()
-    {
-        Time.timeScale = 1;
-        transitionBegin.SetActive(true);
-        tutoImage.SetActive(false);
-        transitionEnd.SetActive(false);
-    }
+	private void Start()
+	{
+		Time.timeScale = 1;
+		transitionBegin.SetActive(true);
+		tutoImage.SetActive(false);
+		transitionEnd.SetActive(false);
+		aSource = gameObject.GetComponent<AudioSource>();
+	}
 
-    public void botonjugar()
-    {
-        transitionEnd.SetActive(true);
-    }
+	public void botonjugar()
+	{
+		transitionEnd.SetActive(true);
+		aSource.Play();
+	}
 
-    public void botontuto()
-    {
-        tutoImage.SetActive(true);
-    }
+	public void botontuto()
+	{
+		tutoImage.SetActive(true);
+		aSource.Play();
 
-    public void botonnotuto()
-    {
-        tutoImage.SetActive(false);
-    }
+	}
 
-    public void botonSailr()
-    {
-        Application.Quit();
-    }
+	public void botonnotuto()
+	{
+		tutoImage.SetActive(false);
+		aSource.Play();
+
+	}
+
+	public void botonSailr()
+	{
+		aSource.Play();
+		Application.Quit();
+	}
 }
